41881a253fadde8b82c8a92b1fad64ff034bb3cd
aribornstein <abornst@microsoft.com>
2015-11-16 00:04:42 -0500

    adding code

---
+++ src/JavaAzureMLClient.java
@@ -0,0 +1,49 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class AzureMLClient 
+{
+
+      private endPointURL; //Azure ML Endpoint
+      private key; //API KEY
+
+
+      public AzureMLClient(String endPointURL,String key)
+      {
+        this.endPointURL= endPointURL;
+        this.key= key;
+      }
+      /*
+       Takes an Azure ML Request Body then Returns the Response String Which Contains Scored Lables etc
+      */
+      public static String RequestResponse( String requestBody ) throws Exception
+      {
+        URL u = new URL(this.endPointURL);
+        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+
+        conn.setRequestProperty("Authorization","Bearer "+ this.key);
+        conn.setRequestProperty("Content-Type","application/json");
+        
+        conn.setRequestMethod("POST");
+        String body= new String(requestBody);
+        
+        conn.setDoOutput(true);
+        OutputStreamWriter wr=new OutputStreamWriter(conn.getOutputStream());
+
+        wr.write(body);
+        wr.close();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        
+        String decodedString;
+        String responseString="";    
+
+        while ((decodedString = in.readLine()) != null) 
+    	{
+            response+=decodedString;
+        }
+    	return responseString;
+     }
+
+  }
